1bf628a79b144362cb0201a615e0e6ff80877546
nafihkh <muhammednafih.k.hkottankodan@gmail.com>
2025-06-30 17:00:01 +0530

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,35 @@
 let inputBox = document.getElementById("inputBox");
-let addTask = document.getElementById("button-addon2");
+let addlist = document.getElementById("button-addon2");
 let listContainer = document.getElementById("list-container");
-let listArr = JSON.parse(localStorage.getItem("tasks")) || [];
+let listArr = JSON.parse(localStorage.getItem("storage")) || [];
 let conut;
 
 function addList() {
-    let task = inputBox.value;
-    if(task ==="")
+    let listElement = inputBox.value;
+    if(listElement ==="")
     {
         alert("Please Enter a Task")
     }
     else 
     {
         count = 0;
-        createList(task);
-        savetask(task);
+        createList(listElement);
+        savelist(listElement);
         inputBox.value = "";
     }
 }
-function createList(task) {
+function createList(listElement) {
     let listItem = document.createElement("li");
     let contentBox = document.createElement("div");
     let textBox = document.createElement("input");
     textBox.setAttribute("id",'checkBox')
     let textItem = document.createElement("label");
     textBox.setAttribute("onchange",'checkcheked()')
-    textItem.innerHTML = task;
+    textItem.innerHTML = listElement;
     if(count==1){
-        textItem.innerHTML = task.task;
+        textItem.innerHTML = listElement.text;
     }
-    textBox.checked = task.status; 
+    textBox.checked = listElement.status; 
     let Trach = document.createElement("button");
     let trachBin = document.createElement("img");
     listContainer.append(listItem);
@@ -38,27 +38,27 @@ function createList(task) {
     contentBox.append(textBox);
     contentBox.append(textItem);
     Trach.append(trachBin);
-    listItem.classList.add('d-flex','justify-content-between','p-2','task');
+    listItem.classList.add('d-flex','justify-content-between','p-2','list');
     textBox.type = "checkbox"
     textBox.classList.add('me-2','form-check-input','rounded-circle');
     Trach.classList.add('border-0');
     trachBin.src = "assets/images/trash.svg";
     Trach.setAttribute("onclick",'removeItem(event)')
 }
-function savetask(task) {
+function savelist(listElement) {
     let obj = {};
-    obj.task = task;
+    obj.text = listElement;
     obj.status = false;
     listArr.push(obj);
-    localStorage.setItem("tasks",JSON.stringify(listArr));
+    localStorage.setItem("storage",JSON.stringify(listArr));
 }
 function removeItem(event)  {
     const listItem = event.target.closest("li");
     if (listItem) {
-        const taskText = listItem.querySelector("label").innerText;
+        const listText = listItem.querySelector("label").innerText;
         listItem.remove();
-        listArr = listArr.filter((x) => x.task !== taskText);
-        localStorage.setItem("tasks", JSON.stringify(listArr));
+        listArr = listArr.filter((x) => x.text !== listText);
+        localStorage.setItem("storage", JSON.stringify(listArr));
     }
 } 
 function checkcheked()
@@ -68,9 +68,9 @@ function checkcheked()
     {
         listArr[i].status = checkBox[i].checked;
     }
-    localStorage.setItem("tasks",JSON.stringify(listArr));
+    localStorage.setItem("storage",JSON.stringify(listArr));
 }
-function loadtask(){
+function loadlist(){
     count = 1;
     listArr.forEach(createList);
 }
@@ -87,4 +87,4 @@ function sort(condition){
         filterlistArr.forEach(createList);
     }
 }
-loadtask();
+loadlist();
